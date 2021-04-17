28833a5100b3b038ac3e6b44c988ff4fef268234
Andrey Barkalov <barkadron@mail.ru>
2021-04-17 22:41:46 +0300

    Добавлен тест для лимитов.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,15 @@
 import min from 'lodash/min';
 import max from 'lodash/max';
-import Game, { GRID_SIZE } from '.';
+import Game, { GRID_SIZE, SLIDE_DIRECTION, SlideLimit } from '.';
 
-type SIZE = '3x3' | '4x4' | '5x5';
-
-(<SIZE[]>['3x3', '4x4', '5x5']).forEach((gridSize) => {
+Object.keys(GRID_SIZE).forEach((gridSize) => {
     // describe(`Game with grid size '3x3'.`, () => {
     const scaleExp = parseInt(gridSize, 10);
 
-    const scale = GRID_SIZE[gridSize];
+    const scale = GRID_SIZE[gridSize as keyof typeof GRID_SIZE];
     const game = new Game({ size: scale });
     const grid = game.getGrid();
+    const limit = game.getSlideLimit();
 
     test(`Grid size '${gridSize}' is equal to expected scale '${scaleExp}'.`, () => {
         expect(scale).toEqual(scaleExp);
@@ -41,6 +40,44 @@ type SIZE = '3x3' | '4x4' | '5x5';
         const emptyCellIndex = game.getEmptyCellIndex();
         expect(emptyCellIndex).toBeGreaterThanOrEqual(0);
         expect(emptyCellIndex).toBeLessThanOrEqual(scale * scale - 1);
+    });
+
+    test(`Limits is correct`, () => {
+        const refLim: SlideLimit = {
+            [SLIDE_DIRECTION.LEFT]:
+                scale === GRID_SIZE['3x3']
+                    ? [0, 3, 6]
+                    : scale === GRID_SIZE['4x4']
+                    ? [0, 4, 8, 12]
+                    : scale === GRID_SIZE['5x5']
+                    ? [0, 5, 10, 15, 20]
+                    : [],
+            [SLIDE_DIRECTION.RIGHT]:
+                scale === GRID_SIZE['3x3']
+                    ? [2, 5, 8]
+                    : scale === GRID_SIZE['4x4']
+                    ? [3, 7, 11, 15]
+                    : scale === GRID_SIZE['5x5']
+                    ? [4, 9, 14, 19, 24]
+                    : [],
+            [SLIDE_DIRECTION.UP]:
+                scale === GRID_SIZE['3x3']
+                    ? [0, 1, 2]
+                    : scale === GRID_SIZE['4x4']
+                    ? [0, 1, 2, 3]
+                    : scale === GRID_SIZE['5x5']
+                    ? [0, 1, 2, 3, 4]
+                    : [],
+            [SLIDE_DIRECTION.DOWN]:
+                scale === GRID_SIZE['3x3']
+                    ? [6, 7, 8]
+                    : scale === GRID_SIZE['4x4']
+                    ? [12, 13, 14, 15]
+                    : scale === GRID_SIZE['5x5']
+                    ? [20, 21, 22, 23, 24]
+                    : [],
+        };
+        expect(limit).toStrictEqual<SlideLimit>(refLim);
     });
     // });
 });
